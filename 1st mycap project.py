d7e95c85eab37b465a4280c812b0de55b733c782
feeltheweb <89317240+feeltheweb@users.noreply.github.com>
2021-08-31 00:19:57 +0530

    Add files via upload

---
+++ 1st mycap project.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: aninditha
+"""
+
+from math import pi
+
+r = float(input ("Input the radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
